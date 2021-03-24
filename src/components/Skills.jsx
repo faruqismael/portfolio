541adde0943b94aa58a991ac04b8a73a8b2ee376
faruqismael <farusmith5@gmail.com>
2021-03-24 20:34:43 +0300

    #1 fixed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,44 +1,59 @@
-import React from 'react';
-import '../styles/Skills.css'
-import { data } from '../data'
+import React from "react";
+import "../styles/Skills.css";
+import { data } from "../data";
 
 const Skills = () => {
-    return (
-        <div className='skills'>
-            <div className="skills__skills">
-                <h2>Skills</h2>
-                <div className="skills__list">
-                    {data.skills.map(
-                        skill => {
-                            return (
-                                <div className="skills__single">
-                                    {/*  */}
-                                    {skill.skill === "Typescript" ?
-                                        <img src={skill.icon} alt={skill.skill} style={{ backgroundColor: "white" }} width={100} className="skills__icon" /> :
-                                        <img src={skill.icon} alt={skill.skill} width={100} className="skills__icon" />}
+  return (
+    <div className="skills">
+      <div className="skills__skills">
+        <h2>Skills</h2>
+        <div className="skills__list">
+          {data.skills.map((skill) => {
+            return (
+              <div className="skills__single">
+                {/*  */}
+                {skill.skill === "Typescript" ? (
+                  <img
+                    src={skill.icon}
+                    alt={skill.skill}
+                    style={{ backgroundColor: "white" }}
+                    className="skills__icon"
+                  />
+                ) : (
+                  <img
+                    src={skill.icon}
+                    alt={skill.skill}
+                    width={100}
+                    className="skills__icon"
+                  />
+                )}
 
-                                    <h4>{skill.skill}</h4>
-                                </div>
-                            )
-                        }
-                    )}
-                </div>
-            </div>
-            <div className="skills__tools">
-                <h2>Tools</h2>
-                <div className="skills__list">
-                    {data.tools.map(tool => {
-                        return (
-                            <div className="skills__single">
-                                <img src={tool.icon} alt={tool.skill} width={100} className="skills__icon" />
-                                <h4>{tool.skill}</h4>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
+                <h4>{skill.skill}</h4>
+              </div>
+            );
+          })}
         </div>
-    );
-}
+      </div>
+      <div className="skills__tools">
+        <h2>Tools</h2>
+        <div className="skills__list">
+          {data.tools.map((tool) => {
+            return (
+              <div className="skills__single">
+                <img
+                  src={tool.icon}
+                  alt={tool.skill}
+                  width={100}
+                  className="skills__icon"
+                />
+                <h4>{tool.skill}</h4>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Skills;
