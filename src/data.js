7987e8372b0b3ec352d9c5c3e43ce24eb880b411
faruqismael <farusmith5@gmail.com>
2021-04-02 00:23:23 +0300

    added sass

--- src/data.js
+++ src/data.js
@@ -1,91 +1,112 @@
-import { faBootstrap, faCss3, faGit, faGithub, faGithubAlt, faGithubSquare, faHtml5, faJs, faPython, faReact, faVuejs } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faBootstrap,
+  faCss3,
+  faGit,
+  faGithub,
+  faGithubAlt,
+  faGithubSquare,
+  faHtml5,
+  faJs,
+  faPython,
+  faReact,
+  faVuejs,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const data = {
-    skills: [
-
-        {
-            "skill": "HTML5",
-            "icon": "https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg"
-        },
-        {
-            "skill": "CSS",
-            "icon": "https://cdn.worldvectorlogo.com/logos/css3.svg"
-        },
-        {
-            "skill": "Javasript",
-            "icon": "https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg"
-        },
-        {
-            "skill": "Typescript",
-            "icon": "https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg"
-        },
-        {
-            "skill": "Python",
-            "icon": "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg"
-        },
-        {
-            "skill": "ReactJS",
-            "icon": "https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg"
-        },
-        {
-            "skill": "Django",
-            "icon": "https://www.vectorlogo.zone/logos/djangoproject/djangoproject-icon.svg"
-        },
-        {
-            "skill": "TailwindCSS",
-            "icon": "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
-        },
-        {
-            "skill": "Bootstrap",
-            "icon": "https://www.vectorlogo.zone/logos/getbootstrap/getbootstrap-icon.svg"
-        },
-        // {
-        //     "skill": "HTML5",
-        //     "icon": faHtml5
-        // },
-        // {
-        //     "skill": "CSS",
-        //     "icon": faCss3
-        // },
-        // {
-        //     "skill": "Javasript",
-        //     "icon": faJs
-        // },
-        // {
-        //     "skill": "Python",
-        //     "icon": faPython
-        // },
-        // {
-        //     "skill": "ReactJS",
-        //     "icon": faReact
-        // },
-        // {
-        //     "skill": "VueJS",
-        //     "icon": faVuejs
-        // },
-        // {
-        //     "skill": "Bootstrap",
-        //     "icon": faBootstrap
-        // },
-        // {
-        //     "skill": "GitHub",
-        //     "icon": faGithubSquare
-        // }
-    ],
-    tools: [
-        {
-            "skill": "GitHub",
-            "icon": "https://www.vectorlogo.zone/logos/github/github-icon.svg"
-        },
-        {
-            "skill": "Git",
-            "icon": "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
-        },
-        {
-            "skill": "VSCode",
-            "icon": "https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg"
-        }
-    ]
-}
-
+  skills: [
+    {
+      skill: "HTML5",
+      icon: "https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg",
+    },
+    {
+      skill: "CSS",
+      icon: "https://cdn.worldvectorlogo.com/logos/css3.svg",
+    },
+    {
+      skill: "Javasript",
+      icon:
+        "https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg",
+    },
+    {
+      skill: "Typescript",
+      icon:
+        "https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg",
+    },
+    {
+      skill: "Python",
+      icon:
+        "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg",
+    },
+    {
+      skill: "SaSS",
+      icon: "https://cdn.worldvectorlogo.com/logos/sass-1.svg",
+    },
+    {
+      skill: "ReactJS",
+      icon: "https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg",
+    },
+    {
+      skill: "Django",
+      icon:
+        "https://www.vectorlogo.zone/logos/djangoproject/djangoproject-icon.svg",
+    },
+    {
+      skill: "TailwindCSS",
+      icon:
+        "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+    },
+    {
+      skill: "Bootstrap",
+      icon:
+        "https://www.vectorlogo.zone/logos/getbootstrap/getbootstrap-icon.svg",
+    },
+    // {
+    //     "skill": "HTML5",
+    //     "icon": faHtml5
+    // },
+    // {
+    //     "skill": "CSS",
+    //     "icon": faCss3
+    // },
+    // {
+    //     "skill": "Javasript",
+    //     "icon": faJs
+    // },
+    // {
+    //     "skill": "Python",
+    //     "icon": faPython
+    // },
+    // {
+    //     "skill": "ReactJS",
+    //     "icon": faReact
+    // },
+    // {
+    //     "skill": "VueJS",
+    //     "icon": faVuejs
+    // },
+    // {
+    //     "skill": "Bootstrap",
+    //     "icon": faBootstrap
+    // },
+    // {
+    //     "skill": "GitHub",
+    //     "icon": faGithubSquare
+    // }
+  ],
+  tools: [
+    {
+      skill: "GitHub",
+      icon: "https://www.vectorlogo.zone/logos/github/github-icon.svg",
+    },
+    {
+      skill: "Git",
+      icon: "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg",
+    },
+    {
+      skill: "VSCode",
+      icon:
+        "https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg",
+    },
+  ],
+};
